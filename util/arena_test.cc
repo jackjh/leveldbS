c4b5cbdc5b921a7740955c777136699546282fbd
Jack <576567826@qq.com>
2019-02-24 22:22:30 +0800

    arena_test ArenaTest SimpleAllocate

--- util/arena_test.cc
+++ util/arena_test.cc
@@ -12,8 +12,33 @@ TEST(ArenaTest, Empty) {
 
 TEST(ArenaTest, SimpleAllocate) {
     Arena arena;
+    std::vector<std::pair<size_t, char*> > allocated;
     const int N = 100000;
-    Random ran(301);
+    Random rand(301);
+    size_t bytes = 0;
+    for(int i=0; i<N; i++) {
+        size_t s;
+        if(i == 0) {
+            s = 1;
+        }
+        else {
+            s = i;
+        }
+        char* buf;
+        if(rand.OneIn(10)) {
+            buf = arena.AllocateAligned(s);
+        }
+        else {
+            buf = arena.Allocate(s);
+        }
+
+        for(size_t j = 0; j < s; j++) {
+            buf[j] = i % 256;
+        }
+        bytes += s;
+        allocated.push_back(std::make_pair(s, buf));
+        ASSERT_GEQ(arena.MemoryUsage(), bytes);
+    }
 }
 
 }
